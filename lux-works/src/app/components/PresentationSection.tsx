2b7a9a5399d1effc1edfe763935dedeb6147ed1f
lukadulac <lukadulac4@gmail.com>
2025-09-17 10:28:24 +0200

    Refactor code structure for improved readability and maintainability

--- lux-works/src/app/components/PresentationSection.tsx
+++ lux-works/src/app/components/PresentationSection.tsx
@@ -32,26 +32,63 @@ const PresentationSection = () => {
 			</div>
 			<div className="relative mt-[50px] w-full flex justify-center items-center md:mt-2">
 				<div className="absolute top-0 left-0 flex items-center gap-4">
-					<Image src="/waves-icon.svg" alt="Waves" width={90} height={30} />
+					<Image
+						src="/waves-icon.svg"
+						alt="Waves"
+						width={90}
+						height={30}
+						className="w-[60px] h-[20px] md:w-[90px] md:h-[30px]"
+					/>
 				</div>
 				<div className="absolute top-0 right-0">
-					<Image src="/circles-icon.svg" alt="Circles" width={36} height={42} />
+					<Image
+						src="/circles-icon.svg"
+						alt="Circles"
+						width={36}
+						height={42}
+						className="w-[36px] h-[42px] md:w-[51px]] md:h-[60px]"
+					/>
 				</div>
 
-				<div className="relative mt-8">
+				<div className="relative mt-8 flex justify-center items-center">
+					<div
+						className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                  w-[260px] h-[330px] md:w-[310px] md:h-[430px] 
+                  border-2 border-[var(--platinum)] rounded-xl rotate-0 z-0"
+					></div>
+
+					<div
+						className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                  w-[280px] h-[370px] md:w-[350px] md:h-[470px] 
+                  border-2 border-[var(--darkskyblue)] rounded-xl rotate-6 md:rotate-6 z-0"
+					></div>
+
 					<Image
-						src="/hero-image.JPG"
+						src="/hero-image5.png"
 						alt="hero-image"
 						width={300}
 						height={420}
-						className="object-cover rounded-xl relative z-10"
+						className="w-[250px] h-[320px] md:w-[300px] md:h-[420px] 
+               object-cover rounded-xl relative z-10"
 					/>
 				</div>
 
 				{/* Donji elementi */}
 				<div className="absolute z-20 bottom-0  left-1/2 -translate-x-1/2 flex justify-between w-full">
-					<Image src="/play-icon.svg" alt="Play" width={100} height={100} />
-					<Image src="/add-icon.svg" alt="Add" width={50} height={50}  />
+					<Image
+						src="/play-icon.svg"
+						alt="Play"
+						width={100}
+						height={100}
+						className="relative right-4 w-[65px] h-[75px] md:w-[100px] md:h-[100px]"
+					/>
+					<Image
+						src="/add-icon.svg"
+						alt="Add"
+						width={50}
+						height={50}
+						className="relative top-1 w-[40px] h-[40px] md:w-[50px] md:h-[50px]"
+					/>
 				</div>
 			</div>
 		</section>
