f87bd7e966ad657e7787c2c3870407c11fc95193
lukadulac <lukadulac4@gmail.com>
2025-09-19 12:18:05 +0200

    feat: add FuzzyText component and update OurWork page with new design feat: add utility function for class name merging chore: update package dependencies in package.json and package-lock.json

--- lux-works/src/app/ourWork/page.tsx
+++ lux-works/src/app/ourWork/page.tsx
@@ -1,9 +1,12 @@
+import FuzzyText from "../components/FuzzyText";
 
 
 const OurWork = () => {
 	return (
-		<div>
-			<h1>Our Work</h1>
+		<div className="text-center mt-20 w-full h-full flex items-center justify-center">
+			<FuzzyText fontSize={52}  fontWeight="bold" fontFamily="Arial" color="#fff" enableHover={true} baseIntensity={0.2} hoverIntensity={0.5}>
+				Comming Soon
+			</FuzzyText>
 		</div>
 	);
 };
